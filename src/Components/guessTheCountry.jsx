3223751e7b1cfab010bfab945168bac4ea8832c6
Brunopzn <222485171+brunopzn@users.noreply.github.com>
2026-02-10 18:20:47 -0300

    integração com a API restcountries usando fetch

--- src/Components/guessTheCountry.jsx
+++ src/Components/guessTheCountry.jsx
@@ -1,10 +1,53 @@
 import style from './GuessTheCountry.module.css'
+import { useState, useEffect } from 'react';
 
 function GuessTheCountry() {
+    const [country, setCountry] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [chances, setChances] = useState(0);
+    const [score, setScore] = useState(0);
+    const [userInput, setUserInput] = useState('')
+
+
+    useEffect(()=> {
+        setLoading(true)
+        fetch('https://restcountries.com/v3.1/all?fields=name,flags').then(response => {
+            if(!response.ok) {
+                throw new Error('Erro: ' + response.status);
+            }
+            return response.json();
+        }).then(data => {
+            const dataRandom = Math.floor(Math.random() * data.length)
+            const countrySelected = data[dataRandom]
+            setCountry(countrySelected)
+        }).catch(err=> {
+            console.error(err)
+        }).finally(() => {
+            setLoading(false)
+        })
+
+    },[])
+
+    const handleChange = (e) => {
+         
+        setUserInput(e.target.value)
+        
+    }
+
+    const handleClick = () => {
+        
+        if(country.name.common === userInput) {
+            return alert('voce acertou')
+        } else {
+            return alert('voce errou')
+        }
+    }
+
     return (
         <>
             <main>
                 <h1>GUESS THE COUNTRY</h1>
+                {loading && <p>Carregando..</p>}
                 <div className={style.scoreAndChancesContainer}>
                     <div className={style.scoreBox}>
                         <h3>YOUR SCORE</h3>
@@ -18,13 +61,16 @@ function GuessTheCountry() {
                 
                 <div className={style.mainContainer}>
                     <div className={style.imgBox}>
-
+                        {country && country.flags && (
+                            <img src={country.flags.svg} alt="flags of a country" key={country.name.common}/>
+                        )}
+                        
                     </div>
 
-                    <input type="text" />
+                    <input type="text" onChange={handleChange} />
 
                     <div className={style.buttons}>
-                        <button>ANSWER</button>
+                        <button onClick={handleClick}>ANSWER</button>
                         <button>NEXT</button>
 
                     </div>

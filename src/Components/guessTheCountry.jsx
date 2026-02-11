4f4a9af68a98298582ea43dfd4193b497850542c
Brunopzn <222485171+brunopzn@users.noreply.github.com>
2026-02-11 17:51:31 -0300

    Projeto Finalizado

--- src/Components/guessTheCountry.jsx
+++ src/Components/guessTheCountry.jsx
@@ -2,11 +2,14 @@ import style from './GuessTheCountry.module.css'
 import { useState, useEffect } from 'react';
 
 function GuessTheCountry() {
+    const [allCountries, setAllCountries] = useState([])
     const [country, setCountry] = useState(null);
     const [loading, setLoading] = useState(false);
-    const [chances, setChances] = useState(0);
+    const [chances, setChances] = useState(10);
     const [score, setScore] = useState(0);
     const [userInput, setUserInput] = useState('')
+    const [isCorrect, setIsCorrect] = useState('')
+    const [isVisible, setIsVisible] = useState(false)
 
 
     useEffect(()=> {
@@ -17,6 +20,7 @@ function GuessTheCountry() {
             }
             return response.json();
         }).then(data => {
+            setAllCountries(data)
             const dataRandom = Math.floor(Math.random() * data.length)
             const countrySelected = data[dataRandom]
             setCountry(countrySelected)
@@ -29,33 +33,70 @@ function GuessTheCountry() {
     },[])
 
     const handleChange = (e) => {
-         
+         setIsCorrect('')
         setUserInput(e.target.value)
         
     }
 
     const handleClick = () => {
+        const countryLower = country.name.common.toLowerCase().trim();
+        const userInputLower = userInput.toLowerCase().trim();
         
-        if(country.name.common === userInput) {
-            return alert('voce acertou')
+        if(countryLower === userInputLower) {
+            setScore(prevScore => prevScore + 1);
+            setChances(prevChances => prevChances - 1)
+            setIsCorrect('correct')
+            
         } else {
-            return alert('voce errou')
+            setChances(prevChances => prevChances - 1)
+            setIsCorrect('incorrect')
+        }
+        const allCountriesIndex = Math.floor(Math.random() * allCountries.length)
+        setCountry(allCountries[allCountriesIndex])
+        setUserInput('')
+        
+        if(chances === 0) {
+            setIsVisible(true)
+        }
+
+    }
+
+    const handleNext = () => {
+        const allCountriesIndex = Math.floor(Math.random() * allCountries.length)
+        setChances(prevChances => prevChances - 1)
+        setCountry(allCountries[allCountriesIndex])
+        if(chances === 0) {
+            setIsVisible(true)
         }
     }
+
+
+    
 
     return (
         <>
             <main>
+                {isVisible ? (
+                <div className={style.finalGame}>
+                    <h1 className={style.finalGameH1}>The game is over</h1>
+                    <p className={style.finalGameH2}>Your Final Score is {score}/10</p>
+                    <button onClick={() => {setIsVisible(false)}}>Close</button>
+                </div>
+                 ) : (
+                    null
+                 )}
+                
+    
                 <h1>GUESS THE COUNTRY</h1>
                 {loading && <p>Carregando..</p>}
                 <div className={style.scoreAndChancesContainer}>
                     <div className={style.scoreBox}>
                         <h3>YOUR SCORE</h3>
-                        <h2>0</h2>
+                        <h2>{score}</h2>
                     </div>
                     <div className={style.chancesBox}>
                        <h3>CHANCES</h3>
-                       <h2>0</h2>
+                       <h2>{chances}</h2>
                     </div>
                 </div>
                 
@@ -66,14 +107,18 @@ function GuessTheCountry() {
                         )}
                         
                     </div>
-
-                    <input type="text" onChange={handleChange} />
+                        {isCorrect === 'correct' && <p style={{background: 'green', color: 'white'}}>correct answer</p>}
+                        {isCorrect === 'incorrect' && <p style={{background: 'red', color: 'white'}}>incorrect answer</p>}
+                    <input value={userInput} type="text" onChange={handleChange} />
 
                     <div className={style.buttons}>
                         <button onClick={handleClick}>ANSWER</button>
-                        <button>NEXT</button>
-
+                        <button onClick={handleNext}>NEXT</button>
+                        <button onClick={() => {
+                            setChances(10), setScore(0), setIsCorrect('')
+                        }}>RESTART</button>
                     </div>
+
                 </div>
             </main>
         </>

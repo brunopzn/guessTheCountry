3afbdfb634fa376c9ce2c17df800d42af8dbf840
Brunopzn <222485171+brunopzn@users.noreply.github.com>
2026-02-11 18:05:34 -0300

    Ajustes no botão restart para atualizar a bandeira

--- src/Components/guessTheCountry.jsx
+++ src/Components/guessTheCountry.jsx
@@ -12,10 +12,10 @@ function GuessTheCountry() {
     const [isVisible, setIsVisible] = useState(false)
 
 
-    useEffect(()=> {
+    useEffect(() => {
         setLoading(true)
         fetch('https://restcountries.com/v3.1/all?fields=name,flags').then(response => {
-            if(!response.ok) {
+            if (!response.ok) {
                 throw new Error('Erro: ' + response.status);
             }
             return response.json();
@@ -24,29 +24,29 @@ function GuessTheCountry() {
             const dataRandom = Math.floor(Math.random() * data.length)
             const countrySelected = data[dataRandom]
             setCountry(countrySelected)
-        }).catch(err=> {
+        }).catch(err => {
             console.error(err)
         }).finally(() => {
             setLoading(false)
         })
 
-    },[])
+    }, [])
 
     const handleChange = (e) => {
-         setIsCorrect('')
+        setIsCorrect('')
         setUserInput(e.target.value)
-        
+
     }
 
     const handleClick = () => {
         const countryLower = country.name.common.toLowerCase().trim();
         const userInputLower = userInput.toLowerCase().trim();
-        
-        if(countryLower === userInputLower) {
+
+        if (countryLower === userInputLower) {
             setScore(prevScore => prevScore + 1);
             setChances(prevChances => prevChances - 1)
             setIsCorrect('correct')
-            
+
         } else {
             setChances(prevChances => prevChances - 1)
             setIsCorrect('incorrect')
@@ -54,8 +54,8 @@ function GuessTheCountry() {
         const allCountriesIndex = Math.floor(Math.random() * allCountries.length)
         setCountry(allCountries[allCountriesIndex])
         setUserInput('')
-        
-        if(chances <= 1) {
+
+        if (chances <= 1) {
             setIsVisible(true)
         }
 
@@ -65,28 +65,34 @@ function GuessTheCountry() {
         const allCountriesIndex = Math.floor(Math.random() * allCountries.length)
         setChances(prevChances => prevChances - 1)
         setCountry(allCountries[allCountriesIndex])
-        if(chances <= 1) {
+        if (chances <= 1) {
             setIsVisible(true)
         }
     }
 
+    const handleRestart = () => {
+        setChances(10)
+        setScore(0)
+        setIsCorrect('')
+        const allCountriesIndex = Math.floor(Math.random() * allCountries.length)
+        setCountry(allCountries[allCountriesIndex])
+    }
 
-    
 
     return (
         <>
             <main>
                 {isVisible ? (
-                <div className={style.finalGame}>
-                    <h1 className={style.finalGameH1}>The game is over</h1>
-                    <p className={style.finalGameH2}>Your Final Score is {score}/10</p>
-                    <button onClick={() => {setIsVisible(false)}}>Close</button>
-                </div>
-                 ) : (
+                    <div className={style.finalGame}>
+                        <h1 className={style.finalGameH1}>The game is over</h1>
+                        <p className={style.finalGameH2}>Your Final Score is {score}/10</p>
+                        <button onClick={() => { setIsVisible(false) }}>Close</button>
+                    </div>
+                ) : (
                     null
-                 )}
-                
-    
+                )}
+
+
                 <h1>GUESS THE COUNTRY</h1>
                 {loading && <p>Carregando..</p>}
                 <div className={style.scoreAndChancesContainer}>
@@ -95,28 +101,26 @@ function GuessTheCountry() {
                         <h2>{score}</h2>
                     </div>
                     <div className={style.chancesBox}>
-                       <h3>CHANCES</h3>
-                       <h2>{chances}</h2>
+                        <h3>CHANCES</h3>
+                        <h2>{chances}</h2>
                     </div>
                 </div>
-                
+
                 <div className={style.mainContainer}>
                     <div className={style.imgBox}>
                         {country && country.flags && (
-                            <img src={country.flags.svg} alt="flags of a country" key={country.name.common}/>
+                            <img src={country.flags.svg} alt="flags of a country" key={country.name.common} />
                         )}
-                        
+
                     </div>
-                        {isCorrect === 'correct' && <p style={{background: 'green', color: 'white'}}>correct answer</p>}
-                        {isCorrect === 'incorrect' && <p style={{background: 'red', color: 'white'}}>incorrect answer</p>}
+                    {isCorrect === 'correct' && <p style={{ background: 'green', color: 'white' }}>correct answer</p>}
+                    {isCorrect === 'incorrect' && <p style={{ background: 'red', color: 'white' }}>incorrect answer</p>}
                     <input value={userInput} type="text" onChange={handleChange} />
 
                     <div className={style.buttons}>
                         <button onClick={handleClick}>ANSWER</button>
                         <button onClick={handleNext}>NEXT</button>
-                        <button onClick={() => {
-                            setChances(10), setScore(0), setIsCorrect('')
-                        }}>RESTART</button>
+                        <button onClick={handleRestart}>RESTART</button>
                     </div>
 
                 </div>
